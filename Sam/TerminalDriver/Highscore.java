c9fd6f572d96caabc219f32138444f8eff87db0a
SamB <samuel.bounds@slu.edu>
2020-03-12 04:34:51 -0500

    Terminal version code

---
+++ Sam/TerminalDriver/Highscore.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Highscore {
+	public void saveHighscore(String name, String score) {
+		try {
+			File highscore = new File("save_highscore.csv");
+
+			FileWriter writer = new FileWriter(highscore);
+	
+			writer.write(name + "," + score);
+			
+			writer.close();
+		}
+
+		catch (IOException e) {
+			System.out.println("Error occured");
+		}
+	}
+
+	public int getHighscore(String filename) {
+		File file = new File(filename);
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			String line = br.readLine();
+			String[] strArray = line.split(",");
+			return Integer.parseInt(strArray[1]);
+		}
+
+		catch (Exception e) {
+			System.out.println(filename + " not open");
+			return 0;
+		}
+	}		
+
+}
--- Sam/TerminalDriver/TerminalDriver.java
+++ Sam/TerminalDriver/TerminalDriver.java
@@ -4,44 +4,16 @@ public class TerminalDriver
 {
     public static void main(String []args)
     {
-        Scanner scan = new Scanner(System.in);
-        Deck deck = new Deck();
-        int moves = 0;
+        TerminalInput userInput = new TerminalInput();
+        boolean play = true;
 
-        System.out.println("Choose a difficulty: (E)asy (M)edium or (H)ard");
-        String difficulty = scan.nextLine();
-
-        if (difficulty.equalsIgnoreCase("E") || difficulty.equalsIgnoreCase("M") || difficulty.equalsIgnoreCase("H"))
-            deck.setDifficulty(difficulty.charAt(0));
-        else
-            System.out.println("Invalid Input");
-
-        String firstChoice,secondChoice;
-
-        while(true)//(deck.cardsLeft()>0) not a real method
+        while(play)
         {
-            deck.display();
-
-            System.out.println("Pick a letter: ");
-            firstChoice = scan.nextLine();
-            Card card1 = deck.findLetter(firstChoice.charAt(0));
-            System.out.println(card1.getValue());
-
-            System.out.println("Pick a letter: ");
-            secondChoice = scan.nextLine();
-            Card card2 = deck.findLetter(secondChoice.charAt(0));
-            System.out.println(card2.getValue());
-
-            moves++;
-            if (card1.compare(card2))
-            {
-                deck.removeCard(card1);
-                deck.removeCard(card2);
-            }
-            else
-                System.out.println("Mismatch");
+            Deck deck = new Deck();
+            userInput.userChoices(deck);
+            userInput.userChooseCard(deck);
+            play = userInput.playAgain();
         }
 
-        //scan.close();
     }
 }
