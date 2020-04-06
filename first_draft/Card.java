62e760eb5869fe98a770de8e3567b3c3628055ba
Long T. Trinh <lttrinh@hopper.slu.edu>
2020-04-05 20:16:42 -0500

    Terminal version of game submission

---
+++ first_draft/Card.java
@@ -0,0 +1,37 @@
+public class Card
+{
+    protected int value;
+    protected char letter;
+
+    public Card(int value, char letter)
+    {
+        this.value = value;
+        this.letter = letter;
+    }
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+    public char getLetter()
+    {
+        return this.letter;
+    }
+
+	public void setLetter(char l) {
+		this.letter = l;
+	}
+
+    public boolean compare(Card c)
+    {
+        if (c.getValue() == this.getValue())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
