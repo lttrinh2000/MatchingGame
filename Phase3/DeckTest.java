fed797f732a7a4af3afb09b03ce80a51929a6691
SamB <samuel.bounds@slu.edu>
2020-04-19 22:46:03 -0500

    Code for phase 3: unit tests

---
+++ Phase3/DeckTest.java
@@ -0,0 +1,50 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import model.*;
+
+public class DeckTest 
+{
+    @Test
+    public void setEasyExpectTrue()
+    {
+        Deck d = new Deck();
+        d.setDifficulty('E');
+        int numExpected = 4;
+        boolean result = (numExpected==d.cardsLeft());
+        assertEquals("setEasyExpectTrue() ", true, result);
+    }
+
+    @Test
+    public void setMediumExpectTrue()
+    {
+        Deck d = new Deck();
+        d.setDifficulty('M');
+        int numExpected = 6;
+        boolean result = (numExpected==d.cardsLeft());
+        assertEquals("setMediumExpectTrue() ", true, result);
+    }
+
+    @Test
+    public void setHardExpectTrue()
+    {
+        Deck d = new Deck();
+        d.setDifficulty('H');
+        int numExpected = 8;
+        boolean result = (numExpected==d.cardsLeft());
+        assertEquals("setHardExpectTrue() ", true, result);
+    }
+
+    @Test
+    public void removeCardExpectTrue()
+    {
+        Deck d = new Deck();
+        d.setDifficulty('M');
+        int numExpected = 5;
+        Card A = d.findLetter('A');
+        d.removeCard(A);
+        boolean result = (numExpected==d.cardsLeft());
+        assertEquals("removeCardExpectTrue() ", true, result);
+    }
+
+}
---
+++ Phase3/model/Deck.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+import java.util.Arrays;
+
+public class Deck
+{
+    Random randomGenerator;
+    private ArrayList<Card> cards = new ArrayList<Card>();
+    private ArrayList<Character> letters = new ArrayList<Character>();
+    private ArrayList<Integer> values = new ArrayList<Integer>();
+    private int numCards;
+
+    public void buildDeck()
+    {
+        for (int i = 0; i < numCards/2; i++)
+        {
+            values.add(i);
+            values.add(i);
+        }
+
+        //System.out.println(values.size() + " cards");
+        Collections.shuffle(values);
+
+        for (int k = 0; k < numCards; k++)
+        {
+            cards.add(new Card(values.get(k).intValue(), (char)(65+k)));
+        }
+    }
+
+    public void setDifficulty(char d)
+    {
+        //Determine number of cards in deck
+        if (d == 'e' | d == 'E')
+        {
+            //System.out.println("User chose: "+ d);
+            numCards = 4;
+        }
+        else if (d == 'm' | d == 'M')
+        {
+            //System.out.println("User chose: "+ d);  
+            numCards = 6;
+        }
+        else if (d == 'h' | d == 'H')
+        {
+            //System.out.println("User chose: "+ d);
+            numCards = 8;
+        }
+        else
+        {
+            System.out.println("User chose: "+ d);
+            System.out.println("Not acceptable input. Defaulting to medium.");  
+            numCards = 6;
+        }
+
+        buildDeck();
+    }  
+
+    public void display()
+    {
+        System.out.println("\nYour Deck:");
+        for(int i = 0; i < numCards; i++)
+        {
+            System.out.print(cards.get(i).getLetter()+" ");
+        }
+        System.out.println("\n");
+    }
+
+    public void removeCard(Card c)
+    {
+        cards.remove(c);
+        numCards--;
+    }
+
+    public Card findLetter(char c)
+    {
+        for (Card x : cards)
+        {
+            if (x.getLetter() == c)
+            {
+                return x;
+            }
+        }
+        System.out.println("Letter not found.");
+        return null;
+    }
+
+    public int cardsLeft()
+    {
+        return cards.size();
+    }
+
+}
