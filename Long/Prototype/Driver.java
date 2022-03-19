2c008fb3d24d755978df498d6c1d32a8e1541ed6
Long Trinh <ltrinh@LAPTOP-SIUS8D07.localdomain>
2022-03-19 00:38:52 -0500

    Delete unnecessary files/directories

--- Long/Prototype/Driver.java
+++
@@ -1,7 +0,0 @@
-public class Driver
-{
-    public static void main(String []args)
-    {
-        Prototype memoryPrototype = new Prototype(400);
-    }
-}
--- Sam/GUI2/Driver.java
+++
@@ -1,9 +0,0 @@
-import controller.*;
-
-public class Driver
-{
-    public static void main(String []args)
-    {
-        GameBoard memoryGameMain = new GameBoard(400);
-    }
-}
